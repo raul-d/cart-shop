d8f79d668bb2e5d49a111a8f85a2644caef52c0d
raul <raul.duran@globant.com>
2021-01-06 19:57:29 -0600

    Added Home page to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-import { Header, ItemList } from './components';
+import { Header } from './components';
+import { Home } from './pages';
 
 import './App.css';
 
 function App() {
-  const [products, setProduts] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8080/products')
-      .then(res => res.json())
-      .then(res => setProduts(res.products))
-      .catch((e) => console.log(e));
-  }, []);
-
   return (
     <div>
       <Header />
-      <ItemList items={products} />
+      <Home />
     </div>
   );
 }
