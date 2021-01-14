bb017f70f6b0fd52a945f9d353286bba5a903f0c
raul <raul.duran@globant.com>
2021-01-14 12:51:16 -0600

    Adding router to app and create home route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Switch, Route } from 'react-router-dom';
+
 import { Header } from './components';
 import { Home } from './pages';
 
@@ -9,7 +11,9 @@ function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <Switch>
+        <Route exact='/' component={Home}/>
+      </Switch>
     </div>
   );
 }
