b89fe24ff3c04675cb062d4f3478a1c1d5ea0652
raul <raul.duran@globant.com>
2021-01-14 23:23:14 -0600

    Added test to app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { Header } from './components';
+import { Home } from './pages';
+
+import { MemoryRouter } from 'react-router-dom';
+import { shallow, mount } from 'enzyme';
+
+describe('rendering components', () => {
+  it('renders App component without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders Header component without crashing', () => {
+    shallow(<Header />);
+  });
+
+  it('rendering Home page component without crashing', () => {
+    shallow(<Home />);
+  });
+});
+
+describe('passing valid paths to router', () => {
+  it('passig home path', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(wrapper.find(Home)).toHaveLength(1);
+  });
 });
